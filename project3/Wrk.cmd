3c4d365b487caeb88114a54108831b9fe28c2b2c
derfsubterfuge@gmail.com <tolth@tolth-HP.(none)>
2013-02-04 22:17:47 -0800

    project 3 initial update

--- project3/Wrk.cmd
+++
@@ -1,9 +0,0 @@
-set cse451tools=o:\cse\courses\cse451\13wi\tools
-set arch=x86
-set path=%cse451tools%\x86;%cse451tools%\debugging tools for windows;%path%;
-
-alias winbag    "windbg -k com:pipe,port=\\.\pipe\ws03com2"
-alias mount     "vhdmount /p \"%USERPROFILE%\My Documents\My Virtual Machines\cse451Project1.vhd\""
-alias dismount  "vhdmount /u /c all"
-
-
